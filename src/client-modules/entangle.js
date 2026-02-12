b6b297a5ac5cb0d012eef5b4df97d7ca77d9e2ff
rukshan <arkruka@gmail.com>
2026-02-12 16:05:39 +0100

    refactor: :recycle: removed old widows dispatch event

--- src/client-modules/entangle.js
+++ src/client-modules/entangle.js
@@ -1,3 +1,9 @@
+/**
+ * This module will add the external javascript and css file to add the search
+ * widget to the documentation website
+ * Also this adds the google recaptcha code to avoid bot submissions
+ */
+
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 if (ExecutionEnvironment.canUseDOM) {
   // Load Vue component
@@ -6,18 +12,13 @@ if (ExecutionEnvironment.canUseDOM) {
   const link = document.createElement("link");
   link.rel = "stylesheet";
   link.type = "text/css";
-  link.href = "http://localhost:6969/assets/index-CGJhef7l.css";
+  link.href = "https://hicafe.co/style/00fe12f9-513c-45f5-b357-c32f4e7b7ae1.css";
   document.head.appendChild(link);
   const script = document.createElement("script");
-  script.src = "http://localhost:6969/assets/index-CEo8Ge-z.js";
+  script.src = "https://hicafe.co/script/fa0f80c0-6d03-432f-ba09-33e250e1b539.js";
   document.body.appendChild(script);
-
-  // Dispatch event on document instead of window
-  window.triggerVueAction = () => {
-    document.dispatchEvent(
-      new CustomEvent("toggle-entangle-model", {
-        detail: { source: "navbar" },
-      }),
-    );
-  };
+  const recaptcha = document.createElement("script")
+  recaptcha.src = "https://www.google.com/recaptcha/api.js?render=6LdBIyEsAAAAAP7K7mufri9od9pJNNtwlppAGLxb"
+  document.head.appendChild(recaptcha)
+  window.siteKey = "6LdBIyEsAAAAAP7K7mufri9od9pJNNtwlppAGLxb"
 }
